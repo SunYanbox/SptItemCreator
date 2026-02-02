135ab4177f00cd5a74eb5bf9efea1d648dc690e4
Suntion <tasunyanbo@xauat.edu.cn>
2026-02-02 18:29:04 +0800

    <chore> 更新版本号至v0.0.4
    - 将项目版本从0.0.3升级到0.0.4
    - 更新ModMetadata中的版本信息，保持一致
    - 准备发布新版本

--- SptItemCreator/ModMetadata.cs
+++ SptItemCreator/ModMetadata.cs
@@ -12,7 +12,7 @@ public record ModMetadata : AbstractModMetadata, IModWebMetadata
     public override string Name { get; init; } = "SptItemCreator";
     public override string Author { get; init; } = "Suntion";
     public override List<string>? Contributors { get; init; } = [];
-    public override SemanticVersioning.Version Version { get; init; } = new("0.0.3");
+    public override SemanticVersioning.Version Version { get; init; } = new("0.0.4");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.8");
     
     
